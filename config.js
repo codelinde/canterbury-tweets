a157943cf13329cefb2bcc05669c72f020a11bfa
codelinde <codelinde@users.noreply.github.com>
2017-02-23 17:35:18 -0700

    Initial upload of files

---
+++ config.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+  consumer_key:         'udCdLwoGRMXxigjBr0yp6uQuJ',
+  consumer_secret:      'XodG4YMQKERNdPTexinhbsn4to6cZ9FORiVt8KdtFa2QHObDPy',
+  access_token:         '832675818945916928-q00xwblzxhWjHaslcGMbRevvw1A5FQG',
+  access_token_secret:  'MgG4SrCKVPSErpbsLrAoks9lV1W7LXc79z120We5FNd8Y',
+}
